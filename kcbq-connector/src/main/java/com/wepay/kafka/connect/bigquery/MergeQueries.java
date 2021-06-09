ee06d83500bac544945afc33645fe7095a810994
Vignesh Pichaikaran - vkp0005 <Vignesh.Pichaikaran@walmartlabs.com>
2021-06-09 19:37:04 +0530

    logger info is added

--- kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/MergeQueries.java
+++ kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/MergeQueries.java
@@ -280,7 +280,9 @@ public class MergeQueries {
     final String i = INTERMEDIATE_TABLE_ITERATION_FIELD_NAME;
     final String value = INTERMEDIATE_TABLE_VALUE_FIELD_NAME;
     final String batch = INTERMEDIATE_TABLE_BATCH_NUMBER_FIELD;
-
+    
+    logger.info("table_details{}",table(destinationTable));
+    
     return "MERGE " + table(destinationTable) + " "
         + "USING ("
           + "SELECT * FROM ("
