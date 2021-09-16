3c7101d82a87fd6e69a0fa5dd5423eccfad540fa
Vignesh Pichaikaran - vkp0005 <Vignesh.Pichaikaran@walmartlabs.com>
2021-09-16 10:05:35 +0530

    Bigquery Connector exception added

--- kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/write/row/BigQueryWriter.java
+++ kcbq-connector/src/main/java/com/wepay/kafka/connect/bigquery/write/row/BigQueryWriter.java
@@ -134,12 +134,14 @@ public abstract class BigQueryWriter {
         mostRecentException = err;
         if (err.getCode() == INTERNAL_SERVICE_ERROR
             || err.getCode() == SERVICE_UNAVAILABLE
-            || err.getCode() == BAD_GATEWAY) {
+            || err.getCode() == BAD_GATEWAY
+        || err.getLocalizedMessage()== "Connection reset") {
           // backend error: https://cloud.google.com/bigquery/troubleshooting-errors
           /* for BAD_GATEWAY: https://cloud.google.com/storage/docs/json_api/v1/status-codes
              todo possibly this page is inaccurate for bigquery, but the message we are getting
              suggest it's an internal backend error and we should retry, so lets take that at face
              value. */
+          logger.info("BQ Eroor:{}",err.getLocalizedMessage());
           logger.warn("BQ backend error: {}, attempting retry", err.getCode());
           retryCount++;
         } else if (err.getCode() == FORBIDDEN
